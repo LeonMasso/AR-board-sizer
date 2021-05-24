56198306254bad0396f6f202ca79d2dbc887aa0a
SS19-Leon Masso <leon.masso@student.hs-neu-ulm.de>
2021-05-24 15:45:40 +0200

    shoe positioning fix 12

--- main.js
+++ main.js
@@ -360,14 +360,14 @@ $("#positionSlider").on("input change", function() {
 
     if(curposValue == 1){
         $("#positionInd").text("left")
-        shoeModel.translateX((currentBoardMin/2) - currentshoeMax)
+        shoeModel.translateX((currentBoardMin/2) - (currentshoeMax*2))
     }
     else if(curposValue == 2){
         $("#positionInd").text("center")
     }
     else if(curposValue == 3){
         $("#positionInd").text("right")
-        shoeModel.translateX(currentBoardMax + currentshoeMin)
+        shoeModel.translateX(currentBoardMin + currentshoeMin)
     }
 })
 
